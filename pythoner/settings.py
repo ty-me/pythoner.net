f681b7d32189e1ed666c53678e436c0205dcb6a3
TY@MacBook Pro <admin@pythoner.net>
2012-11-19 23:18:41 +0800

    x

--- pythoner/settings.py
+++ pythoner/settings.py
@@ -21,7 +21,7 @@ if os.getenv('site') == 'pythoner':
             'ENGINE': 'django.db.backends.mysql', 
             'NAME': 'pythoner_db',     
             'USER': 'pythoner',            
-            'PASSWORD': '', 
+            'PASSWORD': 'XWb6VmRRhuUqT8nc', 
             'HOST': 'localhost',                      
             'PORT': '',                      
             'REDIS': redis.Redis(host='127.0.0.1')
@@ -33,7 +33,7 @@ if os.getenv('site') == 'pythoner':
 else:
     SESSION_ENGINE = 'django.contrib.sessions.backends.db'
     DEBUG = True
-    DOMAIN = 'localhost:8080'
+    DOMAIN = 'localhost:8009'
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.mysql', 
@@ -60,7 +60,7 @@ MEDIA_ROOT = os.path.join(ROOT_PATH,'media')
 MEDIA_URL = '/static/'
 ADMIN_MEDIA_PREFIX = '/media/'
 
-SECRET_KEY = '#x99xvn(!d-ic@b*@0sbdc$jpb+u!%bfj3q-1c0hi@$%moz+y@'
+SECRET_KEY = '#x99xnv(!c-ip@e*@0sbpc$jxb+x!%bfw3f-1c0hi@$%moz+y@'
 
 TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.Loader',
@@ -112,9 +112,9 @@ INSTALLED_APPS = (
     'jobs',
 )
 
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'xxx@gmail.com'
-EMAIL_HOST_PASSWORD = '******'
+EMAIL_HOST = 'smtp.qq.com'
+EMAIL_HOST_USER = 'tianyu0915@qq.com'
+EMAIL_HOST_PASSWORD = 'tyzxwdtys'
 
 CACHE_BACKEND = 'file://'+os.path.join(ROOT_PATH,'cache')
 
@@ -123,17 +123,17 @@ AUTH_PROFILE_MODULE = 'accounts.UserProfile'
 PHOTO_SIZE = (128,128)
 ICON_PHOTO_SIZE = (48,48)
 
-DEFAULT_PASSWORD = '******'
+DEFAULT_PASSWORD = '122126382'
 
-DOUBAN_API_KEY = '******' 
-DOUBAN_API_SECRET = '******' 
+DOUBAN_API_KEY = '09762ab58aac6baa1c628020a00542fe' 
+DOUBAN_API_SECRET = '57d35048a51dfb5d' 
 DOUBAN_SCOPE = 'shuo_basic_r,shuo_basic_w,douban_basic_common' 
 DOUBAN_CALLBACK_URL = u'http://pythoner.net/accounts/login/douban/callback/'
 
-WEIBO_APP_KEY = u'******'
-WEIBO_APP_SECRET = u'******'
+WEIBO_APP_KEY = u'3589637735'
+WEIBO_APP_SECRET = u'2b8b95f3f2ab0f562a3f67752213e0d8'
 WEIBO_CALLBACK_URL = u'http://pythoner.net/accounts/login/sina/callback/'
 
-TWITTER_APP_KEY = u'******'
-TWITTER_APP_SECRET = u'******'
+TWITTER_APP_KEY = u'3589637735'
+TWITTER_APP_SECRET = u'2b8b95f3f2ab0f562a3f67752213e0d8'
 TWITTER_CALLBACK_URL = u'http://pythoner.net/accounts/login/sina/callback/'
