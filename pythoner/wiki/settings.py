601bf25068e581b86f2a3c17b2705d85c36687a9
TY@MacBook Pro <admin@pythoner.net>
2012-11-20 21:59:47 +0800

    x

---
+++ pythoner/wiki/settings.py
@@ -0,0 +1,18 @@
+#encoding:utf-8
+#CMS模块配置文件
+
+# 这是当年从另外一个CMS项目移植过来的，保留了原先的文件结构
+
+#APP(与文件夹名称对应)
+APP = 'wiki'
+
+APP_NAME = '板报'
+
+#列表页显示条目数量
+ENTRY_COUNT = 20
+
+#列表页面模板
+LIST_PAGE = APP+'_list.html'
+
+#详细内容页面模板
+DETAIL_PAGE = APP+'_detail.html'
