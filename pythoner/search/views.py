582001905529a1899c89a104ee7484d15093c88d
TY@MacBook Pro <admin@pythoner.net>
2012-11-20 22:13:52 +0800

    :-0)

--- pythoner/search/views.py
+++ pythoner/search/views.py
@@ -8,7 +8,6 @@ from django.contrib import messages
 from django.template import RequestContext
 from django.views.decorators.csrf import csrf_protect
 from main.verify.views import *
-from others.apis.sphinxapi import *
 from wiki.models import Entry  
 from topic.models import Topic 
 from code.models import Base 
@@ -64,42 +63,4 @@ def search(request,name,page=0):
     except(InvalidPage,EmptyPage):
         entrys = paginator.page(paginator.num_pages)
     return render('search.html',locals(),context_instance=RequestContext(request))
-
-def _search_by_sphinx(request,name):
-    """
-    sphinx 全文搜索，暂时不用
-    """
-    mode = SPH_MATCH_ALL
-    host = 'localhost'
-    port = 9312
-    indexs = {
-            'wiki':{'name':'wiki_entry','module':wiki,'module_name':'板报'},
-            'topic':{'name':'topic_topic','module':topic,'module_name':'话题'},
-            'code':{'name':'code_base','module':code,'module_name':'代码'},
-            'job':{'name':'jobs_job','module':job,'module_name':'招聘'},
-        }
-
-    try:
-        index = indexs[name]
-    except KeyError:
-        return HttpResponse(name)
-        raise Http404()
-    else:
-        sphinx_index = index['name']
-        module = index['module']
-
-    q = str(request.GET.get('q',''))
-    if not q:
-        return Http404()
-    cl = SphinxClient()
-    cl.SetServer(host,port)
-    cl.SetWeights([100,1])
-    cl.SetMatchMode(mode)
-    res = cl.Query(q,sphinx_index)
-    ids = []
-    for re in res['matches']:
-        ids.append(re['id'])
-
-    entrys = module.objects.filter(id__in=ids)
-    return render('search.html',locals())
 
