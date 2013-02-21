2ffd61d4b8b3bb9c120be6b6ec493c4f4f774644
TY@MacBook Pro <admin@pythoner.net>
2013-02-21 21:24:14 +0800

    bug fix

--- pythoner/settings.example.py
+++ pythoner/settings.example.py
@@ -1,49 +1,40 @@
 #encoding:utf-8
 import os
+import sys
 
 ROOT_PATH = os.path.normpath(os.path.dirname(__file__)).replace('\\','/')
 DEFAULT_CHARSET = 'utf8'
 
 ADMINS = (
-          ('admin', 'admin@pythoner.net'),
-          )
+    ('admin', 'admin@pythoner.net'),
+)
 
 MANAGERS = ADMINS
 
-if os.getenv('site') == 'pythoner':
-    # 服务器环境
-    SESSION_ENGINE	= 'django.contrib.sessions.backends.cache'
-    DEBUG = False
-    DOMAIN = 'http://pythoner.net'
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.mysql',
-            'NAME': 'pythoner_db',
-            'USER': 'pythoner',
-            'PASSWORD': '',
-            'HOST': 'localhost',
-            'PORT': '',
-    }
-    }
-    STATIC_ROOT = '/var/pythoner.net/static/'
 
-# 本地环境
-else:
+ENV = os.getenv('ENV')
+
+if ENV in ['DEV']:
+    # 本地环境
+    DEBUG = True
     SESSION_ENGINE = 'django.contrib.sessions.backends.db'
     DEBUG = True
-    DOMAIN = 'localhost:8080'
+    DOMAIN = 'localhost:8009'
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.mysql',
-            'NAME': 'pythoner_db',
-            'USER': 'root',
-            'PASSWORD': '',
-            'HOST': 'localhost',
-            'PORT': '',
+            'ENGINE': 'django.db.backends.mysql', 
+            'NAME': 'pythoner_db',      
+            'USER': 'root',               
+            'PASSWORD': '',                
+            'HOST': 'localhost',            
+            'PORT': '',                      
+        }
     }
-    }
-    
-    STATIC_ROOT = os.path.join(ROOT_PATH,'static')
+else:
+    pass
+
+
+STATIC_ROOT = os.path.join(ROOT_PATH,'static')
 
 TEMPLATE_DEBUG = DEBUG
 TIME_ZONE = 'Asia/Shanghai'
@@ -57,61 +48,59 @@ MEDIA_ROOT = os.path.join(ROOT_PATH,'media')
 MEDIA_URL = '/static/'
 ADMIN_MEDIA_PREFIX = '/media/'
 
-SECRET_KEY = '#x99xvn(!d-ic@b*@0sbdc$jvi+n!%bjj2q-1u0ip@$%nob+z@'
+SECRET_KEY = 'xxxxxx'
 
 TEMPLATE_LOADERS = (
-                    'django.template.loaders.filesystem.Loader',
-                    'django.template.loaders.app_directories.Loader',
-                    )
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+)
 
 MIDDLEWARE_CLASSES = (
-                      'django.middleware.common.CommonMiddleware',
-                      'django.contrib.sessions.middleware.SessionMiddleware',
-                      #'django.middleware.csrf.CsrfViewMiddleware',  # 去掉这行
-                      'django.middleware.csrf.CsrfResponseMiddleware', # must before ViewMiddle
-                      #'CsrfResponseMiddleware',
-                      'django.contrib.auth.middleware.AuthenticationMiddleware',
-                      'django.contrib.messages.middleware.MessageMiddleware',
-                      )
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'pythoner.utils.middlewares.PreventWatering',
+)
+
 
 TEMPLATE_CONTEXT_PROCESSORS = (
-                               'django.core.context_processors.auth',
-                               'django.core.context_processors.debug',
-                               'django.core.context_processors.i18n',
-                               'django.core.context_processors.media',
-                               )
+    'django.contrib.messages.context_processors.messages',
+    'django.contrib.auth.context_processors.auth',
+)
 
 ROOT_URLCONF = 'pythoner.urls'
 
 TEMPLATE_DIRS = (
-                 os.path.join(ROOT_PATH,'templates')
-                 )
+   os.path.join(ROOT_PATH,'templates')
+)
 
 INSTALLED_APPS = (
-                  'django.contrib.auth',
-                  'django.contrib.contenttypes',
-                  'django.contrib.sessions',
-                  'django.contrib.sites',
-                  'django.contrib.messages',
-                  # Uncomment the next line to enable the admin:
-                  'django.contrib.admin',
-                  'django.contrib.comments',
-                  'wiki',
-                  'home',
-                  'books',
-                  'accounts',
-                  'search',
-                  'main',
-                  'pm',
-                  'topic',
-                  'code',
-                  'link',
-                  'jobs',
-                  )
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.sites',
+    'django.contrib.messages',
+    # Uncomment the next line to enable the admin:
+    'django.contrib.admin',
+    'django.contrib.comments',
+    'wiki',
+    'home',
+    'books',
+    'accounts',
+    'main',
+    'pm',
+    'topic',
+    'code',
+    'link',
+    'jobs',
+    'utils',
+)
 
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'xxx@gmail.com'
-EMAIL_HOST_PASSWORD = '******'
+EMAIL_HOST = 'smtp.xx.com'
+EMAIL_HOST_USER = 'xxx'
+EMAIL_HOST_PASSWORD = 'xxxxx'
 
 CACHE_BACKEND = 'file://'+os.path.join(ROOT_PATH,'cache')
 
@@ -120,17 +109,18 @@ AUTH_PROFILE_MODULE = 'accounts.UserProfile'
 PHOTO_SIZE = (128,128)
 ICON_PHOTO_SIZE = (48,48)
 
-DEFAULT_PASSWORD = '******'
-
-DOUBAN_API_KEY = '******' 
-DOUBAN_API_SECRET = '******' 
+DEFAULT_PASSWORD = 'xxxxxx'
+DOUBAN_API_KEY = 'xxxxx' 
+DOUBAN_API_SECRET = 'xxxxxx' 
 DOUBAN_SCOPE = 'shuo_basic_r,shuo_basic_w,douban_basic_common' 
 DOUBAN_CALLBACK_URL = u'http://pythoner.net/accounts/login/douban/callback/'
 
-WEIBO_APP_KEY = u'******'
-WEIBO_APP_SECRET = u'******'
+WEIBO_APP_KEY = u'xxxxxx'
+WEIBO_APP_SECRET = u'xxxxxx'
 WEIBO_CALLBACK_URL = u'http://pythoner.net/accounts/login/sina/callback/'
 
-TWITTER_APP_KEY = u'******'
-TWITTER_APP_SECRET = u'******'
+TWITTER_APP_KEY = u'xxxxxx'
+TWITTER_APP_SECRET = u'xxxxxx'
 TWITTER_CALLBACK_URL = u'http://pythoner.net/accounts/login/sina/callback/'
+
+
