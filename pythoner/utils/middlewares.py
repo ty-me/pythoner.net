3efeb9781028215679b1b81bf581dc1783f6299e
TY@MacBook Pro <tianyu0915@gmail.com>
2013-04-09 16:15:01 +0800

    bug fix

--- pythoner/utils/middlewares.py
+++ pythoner/utils/middlewares.py
@@ -23,23 +23,22 @@ class PreventWatering(object):
         post_times = request.session.get('post_times',0)
 
         # 提交次数是否大于单位时间的最大值
-        if post_times >= 3:
-            _code = request.REQUEST.get('verify','') 
-            code = Code(request)
+        if request.method == 'POST':
+            if post_times >= 3:
+                _code = request.REQUEST.get('verify','') 
+                code = Code(request)
 
-            # 检查用户输入的验证码是否正确
-            if not code.check(_code):
-                return render('verify.html',locals(),context_instance=RequestContext(request))
-            else:
+                # 检查用户输入的验证码是否正确
+                if not code.check(_code):
+                    return render('verify.html',locals(),context_instance=RequestContext(request))
+                else:
+                    request.session['post_times'] = 0
+                    request.session['post_stamp'] = time.time()
+
+            elif timer >= 60:
                 request.session['post_times'] = 0
                 request.session['post_stamp'] = time.time()
-
-        elif timer > 60:
-            request.session['post_times'] = 0
-            request.session['post_stamp'] = time.time()
-
-        
-        if request.method == 'POST':
+            
             request.session['post_times'] = request.session['post_times']+1
             request.session.save()
             print 'request.session',request.session.get('post_times')
