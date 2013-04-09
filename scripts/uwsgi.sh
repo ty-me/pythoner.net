fb736a2ab24c9314bf44061c5232db1ac3e03d17
TY@MacBook Pro <tianyu0915@gmail.com>
2013-04-09 20:14:06 +0800

    bug fix

--- scripts/uwsgi.sh
+++ scripts/uwsgi.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-sh env.sh
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+export PYTHONPATH=$DIR:$PYTHONPATH
+export DJANGO_SETTINGS_MODULE="settings"
+export ENV=$1
+source /usr/local/bin/virtualenvwrapper.sh
+workon pythoner
+
 kill -9 `pgrep -f pythoner.sock`
 sleep 0.5
 uwsgi --daemonize /var/log/uwsgi_pythoner.log --socket /var/run/pythoner.sock --chmod-socket --module django_wsgi --pythonpath /www/pythoner --processes 6
