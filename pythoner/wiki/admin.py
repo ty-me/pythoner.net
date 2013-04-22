b681ea77c94e2aeceb7436077ab84fc206345304
TY@MacBook Pro <tianyu0915@gmail.com>
2013-04-22 13:58:02 +0800

    bug fix

--- pythoner/wiki/admin.py
+++ pythoner/wiki/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from wiki.models import *
 
 class EntryAdmin(admin.ModelAdmin):
-    list_display = ('title','category','sub_time','click_time','public')
+    list_display = ('title','category','sub_time','click_time')
     list_display_links = ('title','category')
 
 class TagAdmin(admin.ModelAdmin):
