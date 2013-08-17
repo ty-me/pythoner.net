2c81bdabe854dcfb78646e9c44761beba306e8d8
TY@MacBook Pro <tianyu0915@gmail.com>
2013-08-18 00:10:24 +0800

    bugfix

--- scripts/setupenv.sh
+++ scripts/setupenv.sh
@@ -2,7 +2,7 @@
 yum install -y libmysqld-dev ibmysqlclient-dev,libfreetype
 yum install -y libjpeg-devel freetype-devel libpng-devel
 easy_install pip
-install virtualenvwrapper
+pip install virtualenvwrapper
 
 source /usr/local/bin/virtualenvwrapper.sh
 mkvirtualenv pythoner
