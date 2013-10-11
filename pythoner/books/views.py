4a2fc946f847ea5c6b624a07b66c33e5fe5e18bb
TY@MacBook Pro <tianyu0915@gmail.com>
2013-10-11 21:46:16 +0800

    update

--- pythoner/books/views.py
+++ pythoner/books/views.py
@@ -27,6 +27,7 @@ from books.models import *
 def list(request,page=1):
     current_page = 'books'
     pre_url = 'books'
+    page_title = u'图书'
     book_all = Book.objects.filter(display=True)
     paginator = Paginator(book_all,20)
     allow_category = True
@@ -46,17 +47,6 @@ def detail(request,isbn):
 
     next = "/books/%s/" %book.isbn
     Book.objects.filter(isbn=isbn).update(click_time=book.click_time+1)
+    page_title = book.name
     return render('book_detail.html',locals(),context_instance=RequestContext(request))
 
-@csrf_protect
-def add(request):
-    pass
-
-def edit(request,book_id):
-    pass
-
-def delete(request,book_id):
-    pass
-
-def category(request,category_id):
-    pass
