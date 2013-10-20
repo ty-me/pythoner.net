3217195816e10f8d31965516c6b99b8205b2c34b
TY@MacBook Pro <tianyu0915@gmail.com>
2013-10-20 11:23:47 +0800

    update

--- pythoner/main/templatetags/main_tags.py
+++ pythoner/main/templatetags/main_tags.py
@@ -1,10 +1,23 @@
 #encoding:utf-8
+"""
+pythoner.net
+Copyright (C) 2013  PYTHONER.ORG
 
-# -*- coding: utf-8 -*-
-# Data:10-7-20 下午6:54
-# Author: T-y(master@t-y.me)
-# File:main_tags
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
 
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+import re
 from django.contrib.comments.models import Comment
 from django.template import Library
 
@@ -13,7 +26,13 @@ register = Library()
 @register.inclusion_tag('latest_comment.tag.html')
 def get_latest_comment(count=6):
     comments = Comment.objects.order_by('-id')[0:count]
-    return {'comments':comments}
+    res = []
+    for c in comments:
+        li = {}
+        li['comment'] =  re.compile(r'<[^>]+>',re.S).sub('',c.comment)
+        li['url']     = c.get_absolute_url()
+        res.append(li)
+    return {'comments':res}
 
 @register.inclusion_tag('sidebar_ads.tag.html')
 def render_sidebar_ads():
